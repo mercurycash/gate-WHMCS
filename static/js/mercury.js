5913e23dcdbb6d08f42e27a888a7aeee83f04f24
Nikolai Dvinskikh <nickdvinskikh@gmail.com>
2021-05-05 20:05:48 +0400

    mercury.js code style fix

--- static/js/mercury.js
+++ static/js/mercury.js
@@ -1,28 +1,24 @@
-$('document').ready(function(){
-    var systemUrl = $('#system-url').data('url');
-    var ajaxUrl = systemUrl + 'mpayment.php'
-    var createTransactionUrl = ajaxUrl + '?ajax_create_transaction=true';
-    var checkTransactionUrl = ajaxUrl + '?ajax_check_transaction=true';
+$("document").ready(function(){
+    var systemUrl = $("#system-url").data("url");
+    var ajaxUrl = systemUrl + "mpayment.php";
+    var createTransactionUrl = ajaxUrl + "?ajax_create_transaction=true";
+    var checkTransactionUrl = ajaxUrl + "?ajax_check_transaction=true";
 
 
-    // let crypto = $(this).data('type');
-
-    let mail = $('#email').data('email'),
-        amount = $('#amount').data('amount'),
-        order_id = $('#order_id').data('order_id'),
-        currency = $('#currency').data('currency'),
-        minbtc = $('#minbtc').data('minbtc'),
-        mindash = $('#mindash').data('mindash'),
-        mineth = $('#mineth').data('mineth'),
-        checkStatusInterval = $('#checkStatusInterval').data('interval');
+    let mail = $("#email").data("email"),
+        amount = $("#amount").data("amount"),
+        order_id = $("#order_id").data("order_id"),
+        currency = $("#currency").data("currency"),
+        minbtc = $("#minbtc").data("minbtc"),
+        mindash = $("#mindash").data("mindash"),
+        mineth = $("#mineth").data("mineth"),
+        checkStatusInterval = $("#checkStatusInterval").data("interval");
 
 
     function successCallback(obj) {
-        console.log('Invoice paid');
-        //go to success payment page
-        window.location = finish_order_url(obj);
+        window.location = finishOrderUrl(obj);
     }
-    function finish_order_url(paymentdata) {
+    function finishOrderUrl(paymentdata) {
         var params = {};
 
         params.finish_order = order_id;
@@ -42,7 +38,7 @@ $('document').ready(function(){
         let url = window.location.pathname;
         let serializedParams = $.param( params );
         if (serializedParams.length > 0) {
-            url += ((url.indexOf('?') === -1) ? '?' : '&') + serializedParams;
+            url += ((url.indexOf("?") === -1) ? "?" : "&") + serializedParams;
         }
         return url;
     }
@@ -51,26 +47,22 @@ $('document').ready(function(){
         checkoutUrl: createTransactionUrl,
         statusUrl: checkTransactionUrl,
         checkStatusInterval: checkStatusInterval,
-        mount: '#mercury-cash',
-        lang: 'en',
+        mount: "#mercury-cash",
+        lang: "en",
         limits: {
             BTC: minbtc,
             ETH: mineth,
             DASH: mindash,
         }
-
-    })
-
+    });
 
     sdk.checkout(amount, currency, mail);
 
-    sdk.on('close', (obj) => {
-        console.log(obj);
-        if(obj.status && (obj.status == 'TRANSACTION_APROVED' )) {
-            status = obj.status;
+    sdk.on("close", (obj) => {
+        if(obj.status && (obj.status === "TRANSACTION_APROVED" )) {
             successCallback(obj);
         }else{
-             window.history.go(-1)
+             window.history.go(-1);
         }
     });
 });
